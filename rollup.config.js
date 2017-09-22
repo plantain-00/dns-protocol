845b11e448e14c4187bc64616a43b81736fecef2
york yao <yaoao12306@outlook.com>
2017-09-22 15:29:49 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import uglify from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'dist/browser/browser.js',
-  dest: 'dist/dns-protocol.min.js',
-  format: 'umd',
-  moduleName: 'DnsProtocol',
-  plugins: [resolve(), uglify()]
+  input: 'dist/browser/browser.js',
+  name: 'Message',
+  plugins: [resolve(), uglify()],
+  output: {
+    file: 'dist/dns-protocol.min.js',
+    format: 'umd'
+  }
 }
