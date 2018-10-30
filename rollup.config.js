8e7bdec4f84b1b4fd9f81f55a94a5c85c4b86154
york yao <yaoao12306@outlook.com>
2018-10-30 22:50:22 +0800

    chore: add type-coverage check

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import commonjs from 'rollup-plugin-commonjs'
 export default {
   input: 'dist/browser/index.js',
   name: 'Message',
-  plugins: [resolve(), uglify(), commonjs()],
+  plugins: [resolve({ browser: true }), uglify(), commonjs()],
   output: {
     file: 'dist/dns-protocol.min.js',
     format: 'umd'
