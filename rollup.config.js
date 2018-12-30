bc097021d8320161254847b56bd8397f8f7d6e0d
york yao <yaoao12306@outlook.com>
2018-12-30 09:19:44 +0800

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,9 @@ import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   input: 'dist/browser/index.js',
-  name: 'Message',
   plugins: [resolve({ browser: true }), uglify(), commonjs()],
   output: {
+    name: 'Message',
     file: 'dist/dns-protocol.min.js',
     format: 'umd'
   }
