acc2d64d9fbe24fb1bd2ea8eca2caf92fc7f9250
York Yao <me@yorkyao.com>
2020-01-25 12:57:10 +0800

    fix: rollup warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { uglify } from 'rollup-plugin-uglify'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'dist/browser/index.js',
